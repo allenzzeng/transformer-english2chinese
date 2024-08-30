256b9260684553d63ef23d6afdc9896d9570607a
xin1347764176 <115351005+xin1347764176@users.noreply.github.com>
2024-08-30 17:47:34 +0800

    Update transformer_nmt.py

--- transformer_nmt.py
+++ transformer_nmt.py
@@ -18,7 +18,7 @@ from torch.autograd import Variable
 PAD = 0                             # padding占位符的索引
 UNK = 1                             # 未登录词标识符的索引
 BATCH_SIZE = 128                    # 批次大小
-EPOCHS = 20                         # 训练轮数
+EPOCHS = 2                         # 训练轮数
 LAYERS = 6                          # transformer中encoder、decoder层数
 H_NUM = 8                           # 多头注意力个数
 D_MODEL = 256                       # 输入、输出词向量维数
@@ -26,8 +26,8 @@ D_FF = 1024                         # feed forward全连接层维数
 DROPOUT = 0.1                       # dropout比例
 MAX_LENGTH = 60                     # 语句最大长度
 
-TRAIN_FILE = 'nmt/en-cn/train.txt'  # 训练集
-DEV_FILE = "nmt/en-cn/dev.txt"      # 验证集
+TRAIN_FILE = 'nmt/en-cn/train_mini.txt'  # 训练集
+DEV_FILE = "nmt/en-cn/dev_mini.txt"      # 验证集
 SAVE_DIR = 'save'
 SAVE_FILE = os.path.join(SAVE_DIR, 'model.pt')
 #DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
